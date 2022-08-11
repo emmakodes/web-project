00ac4f00a16716fef34addd5570814f5375ccd16
emmakodes <emmamichael65@gmail.com>
2022-08-11 21:29:54 +0100

    secure secret key

--- myfirstdjangoproject/settings.py
+++ myfirstdjangoproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()  # loads the configs from .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z8@ll@g)#a^%c$@)x)2+u_%sj@w_6oo(%s344(%-^jr7*8o8!k'
+
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
